67123f3e397f8d7d036f6e85a338625caa6778ac
Amit-2024 <asemwal125@gmail.com>
2022-04-09 21:20:27 +0530

    P2 commit

---
+++ Learning Java/src/unit_01_p1/P2_Task01_KeywordsInJava.java
@@ -0,0 +1,10 @@
+package unit_01_p1;
+
+public class P2_Task01_KeywordsInJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Learning Java/src/unit_01_p1/P2_Task02_ConstantsInJava.java
@@ -0,0 +1,10 @@
+package unit_01_p1;
+
+public class P2_Task02_ConstantsInJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
